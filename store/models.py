8d2cadb01da663f718cf3db5601173746c0e1674
alan5454 <siuug17@gmail.com>
2026-05-05 17:33:51 -0700

    finich

--- store/models.py
+++ store/models.py
@@ -31,9 +31,11 @@ class Category(models.Model):
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='upload/', blank=True, null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
+    
 
     owner = models.ForeignKey(
         User,
